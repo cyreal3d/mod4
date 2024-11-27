bd32f75f71cb6daf0b52fbbd1f81cc1b488182f1
cyreal3d <49306222+cyreal3d@users.noreply.github.com>
2024-11-27 14:38:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ const appleImage = new Image();
 appleImage.src = 'apple.png'; // Ensure apple.png is in the same directory
 
 // Gravity and Damping
-let gravity = 2;
-const damping = 0.1;
+let gravity = 1;
+const damping = 0.7;
 
 // Apple Class
 class Apple {
@@ -66,7 +66,7 @@ class Apple {
 
 // Create Apples
 const apples = [];
-const maxApples = window.innerWidth < 768 ? 2: 10; // Fewer apples on smaller screens
+const maxApples = window.innerWidth < 768 ? 5: 20; // Fewer apples on smaller screens
 for (let i = 0; i < maxApples; i++) {
   const size = 50;
   const x = Math.random() * (canvas.width - size);
@@ -234,6 +234,9 @@ function startAnimation() {
   imagesLoaded++;
   if (imagesLoaded === 2) animate(); // Wait for both background and apple images
 }
+
+backgroundImage.onload = startAnimation;
+appleImage.onload = startAnimation;
 
 backgroundImage.onload = startAnimation;
 appleImage.onload = startAnimation;
